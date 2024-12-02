6295e87f454b0c7d463817cead907ae2e1708d49
eddy <eddy.mwaniki.27@gmail.com>
2024-12-02 15:43:43 +0300

    modifies index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+function receivesAFunction(callback) { return callback()} 
+function returnsANamedFunction() {return function named(){console.log ("I am Eddy");}}
+function returnsAnAnonymousFunction() {return function (){return 'Anonymous'}}
